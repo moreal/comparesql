56d55a74d0dba3ac81c82d9dd4b28ccd5d49abc0
Lee Dogeon <dev.moreal@gmail.com>
2025-08-01 10:02:31 +0900

    Update title

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -9,7 +9,7 @@ export default function App() {
     <Router
       root={(props) => (
         <MetaProvider>
-          <Title>SolidStart - Basic</Title>
+          <Title>comparesql</Title>
           <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
